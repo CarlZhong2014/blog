d1ffe2d76be93e8b3c2128179a843fcdb6e768cb
carlzhong2014 <carlzhong2014@gmail.com>
2017-03-21 22:40:10 +0800

    article app

--- article/views.py
+++ article/views.py
@@ -2,5 +2,12 @@
 from backend.models import Article, Category
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Create your views here.
 
+
+def home(request):
+    navList = Category.objects.filter(nav_status=True)
+    articleList = Article.objects.all()
+    return render(request, "home.html", {"navList": navList, "articles": articleList})
